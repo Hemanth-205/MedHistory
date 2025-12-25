71dc3922e08e7763290703412a973a7f0a5769dc
HEMANTH G <grhemanth26@gmail.com>
2025-12-25 23:05:27 +0530

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'medhistory-v1';
+const ASSETS_TO_CACHE = [
+    'introduction.html',
+    'offline.html',
+    'css/style.css',
+    'js/supabase-config.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => caches.match('offline.html'));
+        })
+    );
+});
